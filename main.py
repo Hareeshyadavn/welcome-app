82f89ea0448c05187751d5fa405518dc425af1ee
Hareesh <hareesh1692@gmail.com>
2024-03-13 16:20:06 -0500

    add files

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+import socket, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def print_name():
+    return 'welcome to 2022'
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port="8080")
